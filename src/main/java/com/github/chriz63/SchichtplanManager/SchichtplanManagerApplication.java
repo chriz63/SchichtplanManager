695dab6b65a497af295569daa6034f2e7ee23be7
Christopher Menzel <chmenzel567@gmail.com>
2025-10-01 15:59:57 +0200

    sets up new spring boot project

---
+++ src/main/java/com/github/chriz63/SchichtplanManager/SchichtplanManagerApplication.java
@@ -0,0 +1,13 @@
+package com.github.chriz63.SchichtplanManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchichtplanManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchichtplanManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/chriz63/SchichtplanManager/SchichtplanManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.chriz63.SchichtplanManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchichtplanManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
